56e6718748b1abfd44351bd4555a244c4dd948bb
shubham-forvia <shubham.singh@forvia.com>
2026-07-02 15:45:59 +0530

    Initial Commit

---
+++ helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Required for std::cout
+
+int main() {
+    // Output message to the console
+    std::cout << "Hello, World!" << std::endl;
+    return 0; // Indicate successful program termination
+}
